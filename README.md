15f40e4a126894dd5810f3109d0863c235e314aa
Jorge Farfan <morfyo_13@hotmail.com>
2015-10-12 21:01:34 -0300

    Ultimo del 12/10/2015

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Trabajo Final, entregas, documentos, etc de la materia DSSD 2015
 Mail para el grupo: dssd2015.grupo11@gmail.com
 
 
+##Links de ayuda
+
+WebServiceRestFull
+
+http://www-inf.it-sudparis.eu/SIMBAD/courses/doku.php?id=teaching_assistant:workflow:getting_data_from_a_simple_rest_service
+
+
---
+++ WebService/PersonasEjemploWebService.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v8.61 
+MySQL - 5.6.24 : Database - datos
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`datos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `datos`;
+
+/*Table structure for table `persona` */
+
+DROP TABLE IF EXISTS `persona`;
+
+CREATE TABLE `persona` (
+  `id` int(50) NOT NULL,
+  `nombre` varchar(60) DEFAULT NULL,
+  `apellido` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `persona` */
+
+insert  into `persona`(`id`,`nombre`,`apellido`) values (1,'pedro','LL'),(2,'juan','PP'),(3,'alan','OO');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
