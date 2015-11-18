29eb35d9067825d31ccdbd92db1163115d6a13db
Jorge Farfan <morfyo_13@hotmail.com>
2015-11-18 20:06:03 -0300

    Ultimo commit Miercoles

--- WebService/WebServiceRestFull/WebApplication1/src/java/domains/RestConf.java
+++ WebService/WebServiceRestFull/WebApplication1/src/java/domains/RestConf.java
@@ -4,12 +4,6 @@ package domains;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author jorge
